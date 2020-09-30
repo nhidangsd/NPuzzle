e06399dcd5912a706aadbeda423be924234cf2b4
Nhi Dang <42393044+nhidangsd@users.noreply.github.com>
2020-09-30 12:15:53 -0700

    Update tiledriver.py

--- tiledriver.py
+++ tiledriver.py
@@ -5,15 +5,13 @@ driver for graph search problem
 """
 We the undersigned promise that we have in good faith attempted to follow the principles of pair programming. Although
 we were free to discuss ideas with others, the implementation is our own. We have shared a common workspace and taken 
-turns at the keyboard for the majority of the work that we are submitting. Furthermore, any non programming portions
-of the assignment were done independently. We recognize that should this not be the case, 
-we will be subject to penalties as outlined in the course syllabus.
+turns at the keyboard for the majority of the work that we are submitting.
 
-Pair Programmer 1 (print & sign your name, then date it)
-BRANDON RENDON 
-09/29/2020
-Pair Programmer 2 (print & sign your name, then date it)
+Pair Programmer 1 
 NHI DANG
+09/29/2020
+Pair Programmer 2
+BRANDON RENDON 
 09/29/2020
 """
 
